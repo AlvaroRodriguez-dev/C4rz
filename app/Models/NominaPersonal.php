12c068c4d35921c392956d8b9900d7abccc520d4
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-16 10:19:27 -0400

    fix: sincronizar modelo personal con tablas rh

--- app/Models/NominaPersonal.php
+++ app/Models/NominaPersonal.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class NominaPersonal extends Model
 {
-    protected $table = 'nomina_personal';
+    protected $table = 'rh_personal';
 
     protected $fillable = [
         'license',
@@ -17,21 +17,21 @@ class NominaPersonal extends Model
 
     public function configuracionesLaborales(): HasMany
     {
-        return $this->hasMany(NominaConfiguracionLaboral::class, 'nomina_personal_id');
+        return $this->hasMany(NominaConfiguracionLaboral::class, 'rh_personal_id');
     }
 
     public function configuracionesSalariales(): HasMany
     {
-        return $this->hasMany(NominaConfiguracionSalarial::class, 'nomina_personal_id');
+        return $this->hasMany(NominaConfiguracionSalarial::class, 'rh_personal_id');
     }
 
     public function conceptos(): HasMany
     {
-        return $this->hasMany(NominaPersonalConcepto::class, 'nomina_personal_id');
+        return $this->hasMany(NominaPersonalConcepto::class, 'rh_personal_id');
     }
 
     public function cuentasPago(): HasMany
     {
-        return $this->hasMany(NominaCuentaPago::class, 'nomina_personal_id');
+        return $this->hasMany(NominaCuentaPago::class, 'rh_personal_id');
     }
 }
