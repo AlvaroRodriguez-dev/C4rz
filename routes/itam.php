cc1e3a47a8016895be6e819ffe8e7b1c943f4084
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-16 00:51:34 -0400

    ITAM: ordenar rutas antes del parametro dinamico

--- routes/itam.php
+++ routes/itam.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\Itam\ItSolicitudController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('itam')->name('itam.')->group(function () {
-    Route::middleware(['permission:it.solicitudes.view'])->group(function () {
-        Route::get('/solicitudes', [ItSolicitudController::class, 'index'])->name('solicitudes.index');
-        Route::get('/solicitudes/{solicitud}', [ItSolicitudController::class, 'show'])->name('solicitudes.show');
-    });
-
     Route::middleware(['permission:it.solicitudes.create'])->group(function () {
         Route::get('/solicitudes/crear', [ItSolicitudController::class, 'create'])->name('solicitudes.create');
         Route::post('/solicitudes', [ItSolicitudController::class, 'store'])->name('solicitudes.store');
         Route::get('/solicitudes/personal/buscar', [ItSolicitudController::class, 'buscarPersonal'])->name('solicitudes.personal.buscar');
+    });
+
+    Route::middleware(['permission:it.solicitudes.view'])->group(function () {
+        Route::get('/solicitudes', [ItSolicitudController::class, 'index'])->name('solicitudes.index');
+        Route::get('/solicitudes/{solicitud}', [ItSolicitudController::class, 'show'])->name('solicitudes.show');
     });
 });
