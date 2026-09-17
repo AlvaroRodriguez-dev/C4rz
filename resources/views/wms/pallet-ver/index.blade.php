96ec3b9d7cd0ff0e65882c10311c77d1266d4904
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-17 16:50:06 -0400

    WMS: corregir carga de estilos del visor de pallet

--- resources/views/wms/pallet-ver/index.blade.php
+++ resources/views/wms/pallet-ver/index.blade.php
@@ -5,7 +5,6 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/js/select2.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/html5-qrcode@2.3.8/html5-qrcode.min.js"></script>
 
