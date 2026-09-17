f445d58e652c970893fedda500a7d483110a3f84
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-17 19:27:42 -0400

    WMS: probar captura QR en alta resolucion

--- resources/views/wms/pallet-ver/index.blade.php
+++ resources/views/wms/pallet-ver/index.blade.php
@@ -200,7 +200,16 @@
             html5QrCode = new Html5Qrcode("qrReader");
             html5QrCode.start(
                 { facingMode: "environment" },
-                { fps: 10, qrbox: 250 },
+                {
+                    fps: 10,
+                    qrbox: 250,
+                    videoConstraints: {
+                        facingMode: { ideal: "environment" },
+                        width: { ideal: 1920, min: 1280 },
+                        height: { ideal: 2560, min: 720 },
+                        frameRate: { ideal: 30, min: 15 }
+                    }
+                },
                 (decodedText) => {
                     html5QrCode.stop();
                     ocultarZoom();
