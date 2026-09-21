8b1a1642a30544419df193aa45361bd1630a8654
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-20 22:30:57 -0400

    ITAM: permitir acceso de SIS-ADMIN a solicitudes

--- routes/itam.php
+++ routes/itam.php
@@ -8,13 +8,13 @@ Route::middleware(['auth'])->prefix('itam')->name('itam.')->group(function () {
         Route::view('/', 'itam.index')->name('index');
     });
 
-    Route::middleware(['permission:it.solicitudes.create'])->group(function () {
+    Route::middleware(['role_or_permission:SIS-ADMIN|it.solicitudes.create'])->group(function () {
         Route::get('/solicitudes/crear', [ItSolicitudController::class, 'create'])->name('solicitudes.create');
         Route::post('/solicitudes', [ItSolicitudController::class, 'store'])->name('solicitudes.store');
         Route::get('/solicitudes/personal/buscar', [ItSolicitudController::class, 'buscarPersonal'])->name('solicitudes.personal.buscar');
     });
 
-    Route::middleware(['permission:it.solicitudes.view'])->group(function () {
+    Route::middleware(['role_or_permission:SIS-ADMIN|it.solicitudes.view'])->group(function () {
         Route::get('/solicitudes', [ItSolicitudController::class, 'index'])->name('solicitudes.index');
         Route::get('/solicitudes/{solicitud}', [ItSolicitudController::class, 'show'])->name('solicitudes.show');
     });
