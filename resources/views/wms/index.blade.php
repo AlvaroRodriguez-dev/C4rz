3ce5fb383918f26148cb78b22169638876cd1bca
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-21 11:56:42 -0400

    feat: agregar acceso a asignacion de almacenes WMS

--- resources/views/wms/index.blade.php
+++ resources/views/wms/index.blade.php
@@ -329,6 +329,21 @@
                 @endcan
 
                 @can('wms.configurar')
+                    <a href="{{ route('wms.usuario-almacenes.index') }}"
+                        class="bg-violet-600 hover:bg-violet-700 active:scale-95 rounded-2xl shadow-md text-white p-6 transition flex flex-col items-center justify-center text-center">
+                        <div class="flex justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM6 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                        </div>
+                        <div class="font-bold mt-3 text-base leading-tight">Usuarios</div>
+                        <div class="text-xs opacity-90">Almacenes WMS</div>
+                    </a>
+                @endcan
+
+                @can('wms.configurar')
                     <a href="{{ route('wms.configurar.index') }}"
                         class="bg-gray-700 hover:bg-gray-800 active:scale-95 rounded-2xl shadow-md text-white p-6 transition flex flex-col items-center justify-center text-center">
                         <div class="flex justify-center">
