865105545c76b009bf44935397732af79f2f65be
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-20 23:05:57 -0400

    feat(ITAM): agregar rutas de evaluacion tecnica

--- routes/itam.php
+++ routes/itam.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Itam\ItEvaluacionController;
 use App\Http\Controllers\Itam\ItSolicitudController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,11 @@ Route::middleware(['auth'])->prefix('itam')->name('itam.')->group(function () {
         Route::get('/solicitudes/crear', [ItSolicitudController::class, 'create'])->name('solicitudes.create');
         Route::post('/solicitudes', [ItSolicitudController::class, 'store'])->name('solicitudes.store');
         Route::get('/solicitudes/personal/buscar', [ItSolicitudController::class, 'buscarPersonal'])->name('solicitudes.personal.buscar');
+    });
+
+    Route::middleware(['role_or_permission:SIS-ADMIN|it.solicitudes.evaluate'])->group(function () {
+        Route::get('/solicitudes/{solicitud}/evaluar', [ItEvaluacionController::class, 'create'])->name('solicitudes.evaluaciones.create');
+        Route::post('/solicitudes/{solicitud}/evaluar', [ItEvaluacionController::class, 'store'])->name('solicitudes.evaluaciones.store');
     });
 
     Route::middleware(['role_or_permission:SIS-ADMIN|it.solicitudes.view'])->group(function () {
