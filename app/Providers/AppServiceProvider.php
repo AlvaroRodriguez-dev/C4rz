971ec72b80fa50d28924482c2e3c708706c7e981
AlvaroRodriguez-dev <alvarorodriguez.dev@gmail.com>
2026-09-26 08:22:50 -0400

    WMS: registrar rutas de paletización

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\Wms\WmsPaletizacionController;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::middleware(['auth', 'permission:wms.produccion.paletizar'])
+            ->prefix('wms/paletizacion')
+            ->name('wms.paletizacion.')
+            ->group(function () {
+                Route::get('/', [WmsPaletizacionController::class, 'index'])->name('index');
+                Route::get('/buscar', [WmsPaletizacionController::class, 'buscar'])->name('buscar');
+                Route::get('/{entrega}', [WmsPaletizacionController::class, 'show'])->name('show');
+                Route::post('/{entrega}', [WmsPaletizacionController::class, 'store'])->name('store');
+            });
     }
 }
